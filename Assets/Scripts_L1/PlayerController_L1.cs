c5a1b1760999e14e97ba70af5805fed9f3d2f314
Phumin Abdul Hameed <phumin786@gmail.com>
2022-09-16 22:27:43 +0700

    Level 1 design

---
+++ Assets/Scripts_L1/PlayerController_L1.cs
@@ -0,0 +1,33 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class PlayerController_L1 : MonoBehaviour
+// {
+//     public CharacterController controller;
+//     private Vector3 direction;
+//     public float speed = 8;
+//     public float jumpForce = 10;
+//     public float gravity = -20;
+    
+//     void Start()
+//     {
+       
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         float hInput = Input.GetAxis("Horizontal");
+//         direction.x = hInput * speed;
+
+//         direction.y += gravity * Time.deltaTime;
+//         if (Input.GetButtonDown("Jump"))
+//         {
+//             direction.y = jumpForce;
+//         }
+//         controller.Move(direction * Time.deltaTime);
+// 	}
+// }
+
+    
